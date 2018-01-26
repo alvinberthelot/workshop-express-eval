c55db67bd09958a60355a705cd3b55a15a62aec5
Alvin Berthelot <alvin.berthelot@webyousoon.com>
2018-01-26 17:30:32 +0100

    Solution exercise 5

--- src/app.js
+++ src/app.js
@@ -4,15 +4,32 @@ const fs = require('fs');
 
 app.get('/bingo', function (req, res) {
 
+  console.log('QUERY', req.query);
+
   fs.readFile('./src/numbers.txt', (err, data) => {
     if (err) {
       res.status(500).send('Impossible to read file !')
     }
     const result = data.toString().split('\n')
       .map(value => parseInt(value))
-      .filter(Number.isInteger)
-      .join(', ');
-    res.send(result);
+      .filter(Number.isInteger);
+
+    if(!req.query.mynumbers) {
+      res.send(`The bingo game is already started and known numbers are ${result.join(', ')}`);
+    } else {
+      const myNumbers = req.query.mynumbers.split(',')
+        .map(value => parseInt(value))
+        .filter(Number.isInteger)
+        .sort((a, b) => a - b);
+      let sortedResult = result.slice();
+      sortedResult.sort((a, b) => a - b);
+      if (myNumbers.toString() === sortedResult.toString()) {
+        res.send('Bingo');
+      } else {
+        res.send(`The bingo game is already started, sorry your numbers doesn't match with known numbers ${result.join(', ')} ; so you can not say Bingo`);
+      }
+    }
+    
   });
 
 });
