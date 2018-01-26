624394e010e1de4c26b04a06c3d4145bec3c5d4f
Alvin Berthelot <alvin.berthelot@webyousoon.com>
2018-01-26 17:15:24 +0100

    Solution exercise 4

--- src/app.js
+++ src/app.js
@@ -1,8 +1,20 @@
 const express = require('express');
 const app = express();
+const fs = require('fs');
 
 app.get('/bingo', function (req, res) {
-  res.send('Bingo');
+
+  fs.readFile('./src/numbers.txt', (err, data) => {
+    if (err) {
+      res.status(500).send('Impossible to read file !')
+    }
+    const result = data.toString().split('\n')
+      .map(value => parseInt(value))
+      .filter(Number.isInteger)
+      .join(', ');
+    res.send(result);
+  });
+
 });
 
 module.exports = app;
