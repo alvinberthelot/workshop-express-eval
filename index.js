d4b4b23e88f39e97fb0efdd848163c9460e75204
Alvin Berthelot <alvin.berthelot@webyousoon.com>
2018-01-26 17:02:05 +0100

    Solution exercise 2

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ let chalk = require('chalk');
 const app = require('./src/app');
 
 // port
-const port = 3000;
+const port = process.argv[2] || 3000;
 
 app.listen(port, function () {
   // start server here
